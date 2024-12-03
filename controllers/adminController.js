6d27ed733e6da3c9bae5de11365bd1094eae9825
Ethicdev07 <akinadejunior76@gmail.com>
2024-12-02 23:40:24 -0800

    add new files

---
+++ controllers/adminController.js
@@ -0,0 +1,16 @@
+
+
+//Api for adding doctor
+
+const addDoctor = async (req, res)=> {
+  try {
+
+    const { name, email, password, speciality, degree,  experience, about,   fees,  address } = req.body;
+    
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+
+export { addDoctor };
